6c9329870641a2320bf0d3ff9fb3fe94cfc6372f
Benjamin Scheib <bcscheib@yahoo.com>
2011-12-16 23:42:04 -1000

    added the basic rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playsay::Application.config.secret_token = '73b8f8029d48926e9efecff8ff0d7ff29f486d5fc9fd19a99c956527f829e078afb3fafeaa7f0a8750977f9eab961e60bad211805ca8baee16190368cd289c67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Playsay::Application.config.session_store :cookie_store, :key => '_playsay_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Playsay::Application.config.session_store :active_record_store
