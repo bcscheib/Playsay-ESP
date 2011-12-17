ae5c75e3a0bd6dc4bc92d8755fe3ae644983231b
Benjamin Scheib <bcscheib@yahoo.com>
2011-12-17 01:12:48 -1000

    added id to login link

--- public/javascripts/playsay.js
+++ public/javascripts/playsay.js
@@ -5,15 +5,22 @@ var Playsay = {
         if (response.authResponse) {
           console.log('Welcome!  Fetching your information.... ');
           FB.api('/me', function(response) {
-            console.log('Good to see you, ' + response.name + '.');
-            FB.logout(function(response) {
-              console.log('Logged out.');
+//            console.log('Good to see you, ' + response.name + '.');
+//            FB.logout(function(response) {
+//              console.log('Logged out.');
+//            });
+            var user_id = response.id;
+            console.log('user id: ', user_id);
+            var query = FB.Data.query('select src from photo where uid={0}',
+                user_id);
+            query.wait(function(rows) {
+              console.log("results: ", rows);
             });
           });
         } else {
           console.log('User cancelled login or did not fully authorize.');
         }
-      }, {scope: 'email'});
+      }, {scope: 'email, photos'});
     });
   }
 };
