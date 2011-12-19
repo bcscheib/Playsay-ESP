22f9370e247700d8d056569246b1d5fdc58b8f60
Benjamin Scheib <bcscheib@yahoo.com>
2011-12-18 19:26:47 -1000

    testing channel file

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,6 +1,6 @@
 class Guess < ActiveRecord::Base
   belongs_to :user
-  after_create :set_matched
+  after_create :set_guess_matched, :set_paired_user_guess_matched
   attr_accessor :pair_passed
 
   private
@@ -10,7 +10,6 @@ class Guess < ActiveRecord::Base
       paired_user_guesses = user.paired_user.guesses.where('photo_src = ? and body = ?', photo_src, body)
       unless paired_user_guesses.empty?
         self.matched = true
-        #paired_user_guesses.first.update_attributes(:matched => true)
         user.add_points
         user.paired_user.add_points
       end
@@ -19,5 +18,12 @@ class Guess < ActiveRecord::Base
       self.save
     end
     self.save
+  end
+
+  def set_paired_user_guess_matched
+    if user.paired_user #paired user might have passed already
+      paired_user_guesses = user.paired_user.guesses.where('photo_src = ? and body = ?', photo_src, body)
+      paired_user_guesses.first.update_attributes(:matched => true)
+    end
   end
 end
