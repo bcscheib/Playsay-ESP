0557d7a9e3d1c37fc1ab7fc264955d2f2346f798
Benjamin Scheib <bcscheib@yahoo.com>
2011-12-18 19:04:13 -1000

    testing channel file

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -6,9 +6,9 @@ class GuessesController < ApplicationController
     respond_to do |format|
       format.json do
          guess_to_return = @current_user.guesses.create(params[:guess])
-         if(matched_guess = @current_user.guesses.where(:matched => true, :photo_src => guess.photo_src))
-            guess_to_return = matched_guess
-         end
+         #if(matched_guess = @current_user.guesses.where(:matched => true, :photo_src => guess.photo_src))
+         #   guess_to_return = matched_guess
+         #end
          render :json => guess_to_return
       end
     end
